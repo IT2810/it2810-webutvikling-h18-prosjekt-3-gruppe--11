c564218ce5e3ead32fa2c9e7a57cd9452f076a97
Minh Ha Do <minhhd@stud.ntnu.no>
2018-10-07 18:45:30 +0200

    added static, swipable list #4

--- project3_gr11/screens/HomeScreen.js
+++ project3_gr11/screens/HomeScreen.js
@@ -1,90 +1,26 @@
-import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import React, { Component } from "react";
+import { Platform, ScrollView, StyleSheet, View, Text } from 'react-native';
 import { WebBrowser } from 'expo';
+import {Container, Content, Header, Title} from 'native-base';
+import { SwipeableList } from "../components/SwipableList";
+import { listData } from "../constants/ToDosListData";
 
-import { MonoText } from '../components/StyledText';
-
-export default class HomeScreen extends React.Component {
+export default class HomeScreen extends Component {
   static navigationOptions = {
     header: null,
   };
 
   render() {
+
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Heiheiheiheihehihiehihehehiheiheheih
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
-      </View>
+      <ScrollView style={styles.container}>
+            <Container>
+            <Content>
+                <SwipeableList todos={listData}/>
+            </Content>
+        </Container>
+      </ScrollView>
     );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
   }
 
   _handleLearnMorePress = () => {
@@ -97,6 +33,13 @@ export default class HomeScreen extends React.Component {
     );
   };
 }
+
+//TEST
+const nativeBaseStyles = StyleSheet.create({
+    container: {
+        alignItems: 'center'
+    }
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -186,3 +129,4 @@ const styles = StyleSheet.create({
     color: '#2e78b7',
   },
 });
+
