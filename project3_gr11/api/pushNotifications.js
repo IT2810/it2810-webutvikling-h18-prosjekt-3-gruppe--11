0163fa703abe4b178870d0c7afbf98a1df922502
Minh Ha Do <minhhd@stud.ntnu.no>
2018-10-17 14:35:13 +0200

    removed an unnecessary line and some code prettifying #6

--- project3_gr11/api/pushNotifications.js
+++ project3_gr11/api/pushNotifications.js
@@ -4,16 +4,7 @@ import { Permissions, Notifications } from 'expo';
 //Date object for selecting time of the day
 let scheduledTime = new Date();
 scheduledTime.setHours(9);
-scheduledTime.setMinutes(0);
-scheduledTime.setSeconds(0);
-
-export function dailyNotification(){
-    console.log("Scheduled notification set to: " + scheduledTime);
-    scheduledNotification();
-
-    //Trigger a local notification immediately, for testing purposes
-    // Notifications.presentLocalNotificationAsync(localNotification);
-}
+// scheduledTime.setMinutes(0);
 
 //TODO: Edit notification description
 const localNotification = {
@@ -33,6 +24,14 @@ const schedulingOptions =  {
     repeat: "day"
 };
 
+export function dailyNotification(){
+    console.log("Scheduled notification set to: " + scheduledTime);
+    scheduledNotification(schedulingOptions);
+
+    //Trigger a local notification immediately, for testing purposes
+    // Notifications.presentLocalNotificationAsync(localNotification);
+}
+
 async function obtainUserFacingNotifPermissionsAsync() {
     let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
     if (permission.status !== 'granted') {
@@ -42,11 +41,11 @@ async function obtainUserFacingNotifPermissionsAsync() {
         }
     }
     return permission;
-};
+}
 
-async function scheduledNotification(){
+async function scheduledNotification(scheduledFor){
     //Check for notification permission
     await obtainUserFacingNotifPermissionsAsync();
     //Schedule notification
-    Notifications.scheduleLocalNotificationAsync(localNotification, schedulingOptions );
-};
+    Notifications.scheduleLocalNotificationAsync(localNotification, scheduledFor );
+}
