30b4264d69a2948d4b8b34fad875794b9714c658
kimdu <kimdu@stud.ntnu.no>
2018-10-19 22:43:04 +0200

    commented code

--- project3_gr11/components/TheList.js
+++ project3_gr11/components/TheList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {ListView, StyleSheet, TextInput, View} from 'react-native';
 import {Button, CheckBox, Icon, List, ListItem, Text } from 'native-base';
-import {removeData, retrieveData, storeData} from "../api/AsyncStorage";
+import { retrieveData, storeData} from "../api/AsyncStorage";
 
 export class TheList extends Component {
     constructor(props) {
@@ -9,8 +9,9 @@ export class TheList extends Component {
         this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
         this.state = {
             task: "",
-            //Takes in an array of to-do's
+            // Array of todos
             listOfTodos: [],
+            // Array with checked todos
             selectedTodoId: [],
         };
     }
@@ -37,25 +38,31 @@ export class TheList extends Component {
         });
     }
 
+    // Add data from inputfield with date and push it in the todolist
     addNewTodoInput = (txt) => {
         if (!(this.state.task === "")) {
             let listOfTodos = this.state.listOfTodos;
             listOfTodos.push({id: new Date(), task: txt});
             storeData('todoData', listOfTodos);
+            // Set the list
             this.setState({listOfTodos: listOfTodos})
         }
     }
 
+    // Takes the uncheck or check a item from the list
+    // Source: https://github.com/GeekyAnts/NativeBase/issues/989
     onCheckBoxPress(id) {
         let tmp = this.state.selectedTodoId;
 
         if ( tmp.includes( id ) ) {
+            // Shrink the list from the rowsId
             tmp.splice( tmp.indexOf(id), 1 );
         } else {
             tmp.push( id );
 
         }
         storeData('checkedTodoData', tmp);
+        // Set the new array above
         this.setState({selectedTodoId: tmp});
 
     if (this.state.listOfTodos.length == this.state.selectedTodoId.length){
@@ -63,7 +70,9 @@ export class TheList extends Component {
 }
     }
 
+    // Deletes the selected item from the list
     deleteRow(secId, rowId, rowMap) {
+        // Grab reference to this row
         rowMap[`${secId}${rowId}`].props.closeRow();
         const newData = [...this.state.listOfTodos];
         newData.splice(rowId, 1);
@@ -72,30 +81,39 @@ export class TheList extends Component {
     }
 
     render() {
+        // Checks if  row is changes and set it as true,
+        // and set the new defines the new related data to the data source
+        // r1 = old, r2 = new (if it's alike then it's true and row is changed)
         const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
         return (
             <View>
                 <TextInput multiline = {true} style={styles.input}
                     placeholder='Enter a task...'
+                    // Set entered text into a state called task
                     onChangeText= {(text) => this.setState({task:text})}
                 />
                 <Button bordered style={styles.button}
+                    // Takes task above as an argument and add it into the array
                     onPress={() =>  this.addNewTodoInput(this.state.task)}>
                     <Text style={styles.btntxt}> add </Text>
                 </Button>
                 <List style={styles.listcontainer}
                     rightOpenValue={-75}
                     enableEmptySections={true}
+                    // Set the datasource to the array with the list of todos
                     dataSource={ds.cloneWithRows(this.state.listOfTodos)}
                     renderRow={(listOfTodos) =>
                     <ListItem>
                         <CheckBox
+                            /* Set a todo to check if the ID is in the array */
                             checked={this.state.selectedTodoId.includes(listOfTodos.id)}
+                            /* Add/delete todo from/to array if it's checked or unchecked */
                             onPress={()=>this.onCheckBoxPress(listOfTodos.id)}
                         />
                         <Text style={styles.textcontainer}>{listOfTodos.task}</Text>
                     </ListItem>}
                     renderRightHiddenRow={(data, secId, rowId, rowMap) =>
+                        /* Takes the references to the selected row and takes it in the function */
                     <Button full danger onPress={_ => this.deleteRow(secId, rowId, rowMap)}>
                     <Icon active name="trash" />
                     </Button>
