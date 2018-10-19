ad22ea1dd1bcd995d3499e2441e8c17b87ca20b0
CatrionaTorklep <catriona.torklep@gmail.com>
2018-10-19 03:37:09 +0200

    Added StyleSheets to the app #11

--- project3_gr11/components/TheList.js
+++ project3_gr11/components/TheList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {ListView, TextInput, View} from 'react-native';
+import {ListView, StyleSheet, TextInput, View} from 'react-native';
 import {Button, CheckBox, Icon, List, ListItem, Text } from 'native-base';
 
 export class TheList extends Component {
@@ -63,15 +63,15 @@ export class TheList extends Component {
         }
         return (
             <View>
-                <TextInput
+                <TextInput multiline = {true} style={styles.input}
                     placeholder='Enter a task...'
                     onChangeText= {(text) => this.setState({task:text})}
                 />
-                <Button bordered
+                <Button bordered style={styles.button}
                     onPress={() =>  this.addNewTodoInput(this.state.task)}>
-                    <Text> add </Text>
+                    <Text style={styles.btntxt}> add </Text>
                 </Button>
-                <List
+                <List style={styles.listcontainer}
                     rightOpenValue={-75}
                     enableEmptySections={true}
                     dataSource={ds.cloneWithRows(this.state.listOfTodos)}
@@ -81,7 +81,7 @@ export class TheList extends Component {
                             checked={this.state.selectedTodoId.includes(listOfTodos.id)}
                             onPress={()=>this.onCheckBoxPress(listOfTodos.id)}
                         />
-                        <Text>{listOfTodos.task}</Text>
+                        <Text style={styles.textcontainer}>{listOfTodos.task}</Text>
                     </ListItem>}
                     renderRightHiddenRow={(data, secId, rowId, rowMap) =>
                     <Button full danger onPress={_ => this.deleteRow(secId, rowId, rowMap)}>
@@ -94,5 +94,30 @@ export class TheList extends Component {
     }
 }
 
+const styles = StyleSheet.create({
+    textcontainer: {
+        margin: 8,
+    },
+    input: {
+        marginRight: 10,
+        marginLeft: 10,
+        marginTop: 10,
+        fontSize: 30,
+    },
+    button: {
+        alignSelf: 'center',
+        width: '90%',
+        height: 60,
+        marginTop: 10,
+        marginBottom: 10,
+
+    },
+    listcontainer: {
+        marginLeft: 20,
+    },
+    btntxt: {
+        marginLeft: '40%',
+    }
+});
 
 
--- project3_gr11/components/TodayPage.js
+++ project3_gr11/components/TodayPage.js
@@ -4,6 +4,7 @@ import { Notifications } from "expo";
 import {Container, Content, Footer, Header, Title} from "native-base";
 import {TheList} from "./TheList";
 import {NotificationSwitch} from "./NotificationSwitch";
+import {StyleSheet} from "react-native";
 
 export default class TodayPage extends React.Component {
     constructor(props) {
@@ -29,15 +30,15 @@ export default class TodayPage extends React.Component {
     render() {
             return (
                 <Container>
-                    <Header transparent style={{backgroundColor: "#f2c413"}}>
-                        <Title>Moren din sin liste</Title>
+                    <Header transparent style={styles.header}>
+                        <Title style={styles.title}>T  O  D  A  Y   L  I  S  T</Title>
                     </Header>
-                    <Content>
+                    <Content style={styles.container}>
                         {/*<AddTodo/>*/}
                         <TheList />
                         {/*Toggle daily reminder*/}
                     </Content>
-                    <Footer>
+                    <Footer style={styles.footer}>
                         <NotificationSwitch toggleNotif={this.toggleNotification}
                                             notifValue={this.state.enableNotification}/>
                     </Footer>
@@ -45,3 +46,20 @@ export default class TodayPage extends React.Component {
             );
         }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+    },
+    footer: {
+        backgroundColor: "#33ccff",
+    },
+    title: {
+        fontSize: 33,
+        marginTop: 13,
+    },
+    header: {
+        height: 100,
+        backgroundColor: "#33ccff",
+    },
+});
